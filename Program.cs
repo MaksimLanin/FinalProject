bf3e817aebc0fc01fad50771128f75cf6fcae9f3
Maksim Lanin <maksimlanin@mail.ru>
2023-02-13 11:55:00 +0300

    Метод сортировки

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 string[] secondArrray = new string[firstArray.Length];
 
 
+
+
+void Sort(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for( int i = 0; i< firstArray.Length; i++)
+    {
+        if(firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+
